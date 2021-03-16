a05ab7d76c55249386e1574319b54645dd5fb2ab
Tetiana Neshcheret <neshcherett10@gmail.com>
2021-03-16 20:21:44 +0200

    Added new list

--- src/com/tneshcheret/Main.java
+++ src/com/tneshcheret/Main.java
@@ -3,6 +3,17 @@ package com.tneshcheret;
 public class Main {
 
     public static void main(String[] args) {
-
+        IntList list = new IntList();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(68);
+        list.add(50);
+        System.out.println(list.toString());
+        System.out.println(list.get(2));
+        list.set(2,100);
+        System.out.println(list.toString());
+        System.out.println(list.size());
     }
 }
